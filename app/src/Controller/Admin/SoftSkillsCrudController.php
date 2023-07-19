920641ba5ab40f1b27a7d12fb0f4751dccf95fab
BKIslem <benkhaled.islem3@gmail.com>
2023-07-19 13:52:39 +0200

    add dashbored

---
+++ app/src/Controller/Admin/SoftSkillsCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\SoftSkills;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class SoftSkillsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return SoftSkills::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
--- app/src/Security/Authenticator.php
+++ app/src/Security/Authenticator.php
@@ -42,10 +42,14 @@ class Authenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        $user = $token->getUser();
+ 
+        if ($user) {
+            return new RedirectResponse($this->urlGenerator->generate('admin'));
+        }
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
